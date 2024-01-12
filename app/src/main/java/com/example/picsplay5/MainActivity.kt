68e006eb7b666eda3ddd52ccfd282a5ed9671226
jaein <jaein@jellybus.com>
2024-01-12 13:54:15 +0900

    [PICSPLAY-2] 카메라 연동 및 원본이미지 불러오기 개선, 인텐트로 서브화면에 이미지 넘기기 구현 필요

--- app/src/main/java/com/example/picsplay5/MainActivity.kt
+++ app/src/main/java/com/example/picsplay5/MainActivity.kt
@@ -5,51 +5,50 @@ package com.example.picsplay5
 import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.example.picsplay5.R
-import com.example.picsplay5.SubActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.example.picsplay5.databinding.ActivityMainBinding
+import java.io.File
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     private var selectedImageUri: Uri? = null
 
+    lateinit var binding: ActivityMainBinding
+    lateinit var filePath: String
 
-    private val cameraLauncher: ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
-                val imageBitmap = data?.extras?.get("data") as Bitmap?
 
-                selectedImageUri = imageBitmap?.let { bitmap ->
-                    try {
-                        applicationContext.contentResolver.insert(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                            ContentValues().apply {
-                                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-                                put(MediaStore.Images.Media.ORIENTATION, 0)
-                            }
-                        )?.also { uri ->
-                            applicationContext.contentResolver.openOutputStream(uri)?.use { outputStream ->
-                                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-                            }
-                        }
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                        null
-                    }
-                }
 
-                startEditingActivity()
-            }
+    private val  requestCameraFileLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+        val calRatio = calculateInSampleSize(
+            Uri.fromFile(File(filePath)),
+            resources.getDimensionPixelSize(R.dimen.imgSize),
+            resources.getDimensionPixelSize(R.dimen.imgSize)
+        )
+        val option = BitmapFactory.Options()
+        option.inSampleSize = calRatio
+        val bitmap = BitmapFactory.decodeFile(filePath, option)
+        bitmap?.let {
+            binding.userImageView.setImageBitmap(bitmap)
         }
+    }
 
 
 
@@ -67,23 +66,104 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val cameraButton: Button = findViewById(R.id.cameraButton)
+//        setContentView(R.layout.activity_main)
+
+//        val cameraButton: Button = findViewById(R.id.cameraButton)
         val galleryButton: Button = findViewById(R.id.galleryButton)
 
-        cameraButton.setOnClickListener {
-            openCamera()
+
+        val requestCameraFileLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            val calRatio = calculateInSampleSize(
+                Uri.fromFile(File(filePath)),
+                resources.getDimensionPixelSize(R.dimen.imgSize),
+                resources.getDimensionPixelSize(R.dimen.imgSize)
+            )
+            val option = BitmapFactory.Options()
+            option.inSampleSize = calRatio
+            val bitmap = BitmapFactory.decodeFile(filePath, option)
+            bitmap?.let {
+                binding.userImageView.setImageBitmap(bitmap)
+            }
         }
+
+
+        binding.cameraButton.setOnClickListener {
+            //camera app......................
+            //파일 준비...............
+
+            Log.i(">",">>>>>>>>>>>>>>>>>>>>>>>L>L>L>L>L>L>L>L>L>L>L>L")
+
+            requestCameraPermission()
+
+        }
+
+
 
         galleryButton.setOnClickListener {
+            Log.i(">","654646844644654984454")
             openGallery()
+            //requestCameraPermission()
         }
+
+    }
+    private fun calculateInSampleSize(fileUri: Uri, reqWidth: Int, reqHeight: Int): Int {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        try {
+            var inputStream = contentResolver.openInputStream(fileUri)
+
+            //inJustDecodeBounds 값을 true 로 설정한 상태에서 decodeXXX() 를 호출.
+            //로딩 하고자 하는 이미지의 각종 정보가 optionsㅊ에 설정 된다.
+            BitmapFactory.decodeStream(inputStream, null, options)
+            inputStream!!.close()
+            inputStream = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        //비율 계산........................
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+        //inSampleSize 비율 계산
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
     }
 
+
     private fun openCamera() {
+        Log.i(">",">>>>>>>>>>>>>>>>>>>>>>>L>L>L>L>L>L>L>L>L>L>L>L openCamera()")
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val file = File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        )
+        filePath = file.absolutePath
+        val photoURI: Uri = FileProvider.getUriForFile(
+            this,
+            "com.example.picsplay5.fileprovider",
+            file
+        )
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraLauncher.launch(intent)
+
+        Log.i(">",">>>>>>>>>>>>>>>>>>>>>ddddddddcccddcccccccx>L $photoURI <<<<<<<>< openCamera()")
+
+
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        requestCameraFileLauncher.launch(intent)
     }
 
     private fun openGallery() {
@@ -97,5 +177,42 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("imagePath", selectedImageUri?.toString())
         Log.i("?"," qwdqwdwqdqwdwqdwqdwqdwqdqwdwqdwqdwqdwdqwdwqdqwdwqdwqdwq>>>>>>>       $selectedImageUri          <<<<<qwdwqdwqdwqdqwdwqdwqdwqdwq")
         startActivity(intent)
+    }
+
+    // MainActivity에서 런타임 권한을 요청하는 부분 추가
+    private val CAMERA_PERMISSION_REQUEST = 100
+
+    private fun requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST
+            )
+        } else {
+            openCamera()
+        }
+    }
+
+
+    // 런타임 권한 요청 결과를 처리하는 부분
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            } else {
+                // 권한이 거부되었을 때의 처리
+                Toast.makeText(this, "카메라 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
